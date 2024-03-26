530465107d7b2c55e3a573f16c70fcd1831084ba
Martyn-Conkling <23250543+Martyn-Conkling@users.noreply.github.com>
2024-03-25 20:41:44 -0700

    adding some additional practice code to display all the prime numbers

---
+++ generate.js
@@ -0,0 +1,52 @@
+// Listen for messages from the main thread.
+// If the message command is "generate", call `generatePrimes()`
+addEventListener("message", (message) => {
+    if (message.data.command === "generate") {
+      generatePrimes(message.data.quota);
+    }
+  });
+  
+  // Generate primes (very inefficiently)
+  function generatePrimes(quota) {
+    function isPrime(n) {
+      for (let c = 2; c <= Math.sqrt(n); ++c) {
+        if (n % c === 0) {
+          return false;
+        }
+      }
+      return true;
+    }
+  
+    const primes = [];
+    const maximum = 1000000;
+    let primesChunk = [];
+  
+    while (primes.length < quota) {
+      const candidate = Math.floor(Math.random() * (maximum + 1));
+      if (isPrime(candidate)) {
+        primes.push(candidate);
+      }
+    }
+  
+    // When we have finished, send a message to the main thread,
+    // including the number of primes we generated.
+    let counter = 1;
+    let chunkHasUnpostedData = false;
+    for(let i = 0; i < primes.length; i++){
+        
+        primesChunk.push(primes[i])
+        chunkHasUnpostedData = true;
+        if(counter === 1000){
+            postMessage(primesChunk);
+            primesChunk = [];
+            counter = 0;
+            chunkHasUnpostedData = false;
+        }
+        counter++;
+    }
+    if(chunkHasUnpostedData){
+        postMessage(primesChunk)
+    }
+    
+  }
+  
